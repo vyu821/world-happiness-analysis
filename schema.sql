2a4baede253b1a8fe19ab1665d90bc1f4b62d30a
he wang <hewang@hedemacbook-pro.attlocal.net>
2020-12-17 19:33:36 -0800

    adding schema.sql

---
+++ schema.sql
@@ -0,0 +1,74 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Happiness_2015" (
+    "Country" varchar   NOT NULL,
+    "Score" decimal   NOT NULL,
+    "Economy" decimal   NOT NULL,
+    "Social_Support" decimal NOT NULL,
+    "Life_Expectancy" decimal   NOT NULL,
+    "Freedom" decimal   NOT NULL,
+    "Perceptions_of_Corruption" decimal   NOT NULL,
+    "Generosity" decimal   NOT NULL,
+    CONSTRAINT "pk_Happiness_2015" PRIMARY KEY (
+        "Country"
+     )
+);
+
+CREATE TABLE "Happiness_2016" (
+    "Country" varchar   NOT NULL,
+    "Score" decimal   NOT NULL,
+    "Economy" decimal   NOT NULL,
+    "Social_Support" decimal NOT NULL,
+    "Life_Expectancy" decimal   NOT NULL,
+    "Freedom" decimal   NOT NULL,
+    "Perceptions_of_Corruption" decimal   NOT NULL,
+    "Generosity" decimal   NOT NULL,
+    CONSTRAINT "pk_Happiness_2016" PRIMARY KEY (
+        "Country"
+     )
+);
+
+CREATE TABLE "Happiness_2017" (
+    "Country" varchar   NOT NULL,
+    "Score" decimal   NOT NULL,
+    "Economy" decimal   NOT NULL,
+    "Social_Support" decimal NOT NULL,
+    "Life_Expectancy" decimal   NOT NULL,
+    "Freedom" decimal   NOT NULL,
+    "Perceptions_of_Corruption" decimal   NOT NULL,
+    "Generosity" decimal   NOT NULL,
+    CONSTRAINT "pk_Happiness_2017" PRIMARY KEY (
+        "Country"
+     )
+);
+
+CREATE TABLE "Happiness_2018" (
+    "Country" varchar   NOT NULL,
+    "Score" decimal   NOT NULL,
+    "Economy" decimal   NOT NULL,
+    "Social_Support" decimal NOT NULL,
+    "Life_Expectancy" decimal   NOT NULL,
+    "Freedom" decimal   NOT NULL,
+    "Perceptions_of_Corruption" decimal   NOT NULL,
+    "Generosity" decimal   NOT NULL,
+    CONSTRAINT "pk_Happiness_2018" PRIMARY KEY (
+        "Country"
+     )
+);
+
+CREATE TABLE "Happiness_2019" (
+    "Country" varchar   NOT NULL,
+    "Score" decimal   NOT NULL,
+    "Economy" decimal   NOT NULL,
+    "Social_Support" decimal NOT NULL,
+    "Life_Expectancy" decimal   NOT NULL,
+    "Freedom" decimal   NOT NULL,
+    "Perceptions_of_Corruption" decimal   NOT NULL,
+    "Generosity" decimal   NOT NULL,
+    CONSTRAINT "pk_Happiness_2019" PRIMARY KEY (
+        "Country"
+     )
+);
+
